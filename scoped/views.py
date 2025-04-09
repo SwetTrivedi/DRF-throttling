7ed8d87f820676ea27d66d62d9f42951cb605d03
SwetTrivedi <swettrivedi981@gmail.com>
2025-04-09 19:25:22 +0530

    throttling

---
+++ scoped/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import  ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView,ListCreateAPIView,RetrieveUpdateAPIView
+from rest_framework.throttling import ScopedRateThrottle
+# Create your views here.
+class StudentList(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    throttle_classes=[ScopedRateThrottle]
+    throttle_scope='viewstu'
+
+class StudentCreate(CreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    throttle_classes=[ScopedRateThrottle]
+    throttle_scope='modifystu'
+
+
+class StudentRetrieve(RetrieveAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    throttle_classes=[ScopedRateThrottle]
+    throttle_scope='viewstu'
+
+class StudentUpdate(UpdateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    throttle_classes=[ScopedRateThrottle]
+    throttle_scope='modifystu'
+
+
+class Studentdelete(DestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    throttle_classes=[ScopedRateThrottle]
+    throttle_scope='modifystu'
